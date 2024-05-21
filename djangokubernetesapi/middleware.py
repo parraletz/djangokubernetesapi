a6f6d76db4bf1b665803fc6a39073601d3d744a7
Alex Parra <jparra@occ.com.mx>
2024-05-20 20:38:17 -0600

    wip: Added otel configuration

---
+++ djangokubernetesapi/middleware.py
@@ -0,0 +1,51 @@
+import logging
+
+from django.http import HttpResponse, HttpResponseServerError
+
+logger = logging.getLogger("healthz")
+
+class HealthCheckMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        if request.method == "GET":
+            if request.path == "/readiness":
+                return self.readiness(request)
+            elif request.path == "/healthz":
+                return self.healthz(request)
+        return self.get_response(request)
+
+    def healthz(self, request):
+        """
+        Returns that the server is alive.
+        """
+        return HttpResponse("OK")
+
+    def readiness(self, request):
+        try:
+            from django.db import connections
+            for name in connections:
+                cursor = connections[name].cursor()
+                cursor.execute("SELECT 1;")
+                row = cursor.fetchone()
+                if row is None:
+                    return HttpResponseServerError("db: invalid response")
+        except Exception:
+            logger.exception(Exception)
+            return HttpResponseServerError("db: cannot connect to database.")
+
+        try:
+            from django.core.cache import caches
+            from django.core.cache.backends.memcached import BaseMemcachedCache
+            for cache in caches.all():
+                if isinstance(cache, BaseMemcachedCache):
+                    stats = cache._cache.get_stats()
+                    if len(stats) != len(cache._servers):
+                        return HttpResponseServerError("cache: cannot connect to cache.")
+        except Exception:
+            logger.exception(Exception)
+            return HttpResponseServerError("cache: cannot connect to cache.")
+
+        return HttpResponse("OK")
